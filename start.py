f649ed156ad1d62c462951ada67d09c4dff60eda
Karan Jadhav <100209902+KJInnovateX@users.noreply.github.com>
2024-10-19 11:33:26 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-from app import app  # Assuming `app.py` defines your Flask app object
+from app import app  # Assuming your Flask app is defined in app.py
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
